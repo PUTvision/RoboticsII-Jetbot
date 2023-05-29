cce4e438903326ea5205a1ebc3421c2dc5fd672c
Bartosz Ptak <bartptak@gmail.com>
2023-05-29 13:40:41 +0200

    Update PUTDriver.py

--- src/PUTDriver.py
+++ src/PUTDriver.py
@@ -1,3 +1,4 @@
+import cv2
 from jetbot import Robot
 
 
@@ -22,3 +23,31 @@ class PUTDriver:
             right_speed += left*self.max_steering
 
         self.robot.set_motors(left_speed=left_speed*self.max_speed, right_speed=right_speed*self.max_speed)
+
+
+def gstreamer_pipeline(
+    sensor_id=0,
+    capture_width=1920,
+    capture_height=1080,
+    display_width=960,
+    display_height=540,
+    framerate=30,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc sensor-id=%d ! "
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            sensor_id,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
