2081383f41da624ce6d4d9ec16314841cf02bda9
Ignacy Stępka <ignacy.stepka@gmail.com>
2024-05-14 13:06:07 +0200

    Fix dataloading

--- src/training/dataloading_utils.py
+++ src/training/dataloading_utils.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch.utils.data import DataLoader, TensorDataset
 import os 
 import pandas as pd
 import cv2
@@ -22,15 +23,19 @@ def load_data(data_folder_path: str, debug: bool = True) -> tuple[list[np.ndarra
     X,Y = [], []
     
     for filename in filenames:
-        targets = pd.read_csv(os.path.join(data_folder_path, filename))
-        Y.append(targets.to_numpy()[:, 1:])
+        targets = pd.read_csv(os.path.join(data_folder_path, filename)).to_numpy()
+        frame_indices = targets[:, 0].astype(int)
+        Y.append(targets[:, 1:])
         
         # Load images from the corresponding folders
         folder = filename.removesuffix('.csv')
         images = []
-        imgs_names = os.listdir(os.path.join(data_folder_path, folder))
-        for img_name in imgs_names:
+        for frame_idx in frame_indices:
+            fram_idx_pad = str(frame_idx).zfill(4)
+            img_name = f'{fram_idx_pad}.jpg'
             img = cv2.imread(os.path.join(data_folder_path, folder, img_name))
+            # Move the channel axis to the first dimension
+            img = np.moveaxis(img, -1, 0)
             images.append(img)
             
         X.append(images)
@@ -59,13 +64,20 @@ def train_test_split(data: list[np.ndarray], labels: list[np.ndarray], test_size
     train_data, test_data = data[:split_idx], data[split_idx:]
     train_labels, test_labels = labels[:split_idx], labels[split_idx:]
     
+    # Flatten the data
+    train_data = np.array([img for run in train_data for img in run])
+    test_data = np.array([img for run in test_data for img in run])
+    train_labels = np.array([label for run in train_labels for label in run])
+    test_labels = np.array([label for run in test_labels for label in run])
+    
+    
     if debug:
-        print(f'Training data size: {len(train_data)}')
-        print(f'Testing data size: {len(test_data)}')
+        print(f'Training data shape: {train_data.shape}, training labels shape: {train_labels.shape}')
+        print(f'Testing data shape: {test_data.shape}, testing labels shape: {test_labels.shape}')
         
     return train_data, test_data, train_labels, test_labels
 
-def create_dataloader(data: list[np.ndarray], labels: list[np.ndarray], batch_size: int = 32, shuffle: bool = True) -> torch.utils.data.DataLoader:
+def create_dataloader(data: np.ndarray, labels: np.ndarray, batch_size: int = 32, shuffle: bool = True) -> DataLoader:
     '''
     Create a DataLoader object from the data and labels.
     
@@ -76,25 +88,9 @@ def create_dataloader(data: list[np.ndarray], labels: list[np.ndarray], batch_si
     `batch_size`: The size of the batch.
     `shuffle`: If True, shuffle the data.
     '''
-    x = torch.tensor(data)
-    y = torch.tensor(labels)
-    dataset = torch.utils.data.TensorDataset(x, y)
-    return torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
+    x = torch.tensor(data, dtype=torch.float32)
+    y = torch.tensor(labels, dtype=torch.float32)
+    dataset = TensorDataset(x, y)
+    return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
 
-    
-
-if __name__ == '__main__':
-    data, labels = load_data(DATASET_PATH, debug=DEBUG)
-    
-    if DEBUG:
-        # Show the first image
-        cv2.imshow('Image', data[0][0])
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-    
-    
-    train_data, test_data, train_labels, test_labels = train_test_split(data, labels, test_size=0.2, debug=DEBUG)    
-
-    train_loader = create_dataloader(train_data, train_labels, batch_size=BATCH_SZIE, shuffle=True)
-    test_loader = create_dataloader(test_data, test_labels, batch_size=BATCH_SZIE, shuffle=False)
     
