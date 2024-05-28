e88993c9d67e9502702839586048c9e9c6a2bb19
Wojciechm-AdapptLimited <wojciech.maczka@protonmail.com>
2024-05-28 15:21:53 +0200

    fix: add MAE metric monitoring

--- src/training/main.py
+++ src/training/main.py
@@ -1,11 +1,12 @@
+from collections import defaultdict
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision.transforms.v2 as transforms
-from torchsummary import summary
 
 from tqdm import tqdm
 from torch.utils.data import DataLoader, random_split
+from torchmetrics.regression import MeanAbsoluteError
 from typing import Tuple
 
 from loss import WeightedMSELoss
@@ -20,36 +21,55 @@ def train_epoch(
     model: nn.Module,
     train_loader: DataLoader,
     loss_fn: nn.Module,
+    metric_fn: nn.Module,
     optimizer: optim.Optimizer,
     device: torch.device,
 ):
     model.train()
     train_loss = 0.0
+    train_metric = 0.0
 
     for X, y in tqdm(train_loader, "batch"):
         X, y = X.to(device), y.to(device)
         optimizer.zero_grad()
+
         y_pred = model(X)
+
         loss = loss_fn(y_pred, y)
+        metric = metric_fn(y_pred, y)
         loss.backward()
         optimizer.step()
+
+        train_metric += metric.item()
         train_loss += loss.item()
 
-    return train_loss / len(train_loader)
+    return train_loss / len(train_loader), train_metric / len(train_loader)
 
 
 def val_epoch(
-    model: nn.Module, val_loader: DataLoader, loss_fn: nn.Module, device: torch.device
+    model: nn.Module,
+    val_loader: DataLoader,
+    loss_fn: nn.Module,
+    metric_fn: nn.Module,
+    device: torch.device,
 ):
     model.eval()
     val_loss = 0.0
+    val_metric = 0.0
+
     with torch.no_grad():
         for X, y in tqdm(val_loader):
             X, y = X.to(device), y.to(device)
+
             y_pred = model(X)
+
             loss = loss_fn(y_pred, y)
+            metric = metric_fn(y_pred, y)
+
             val_loss += loss.item()
-    return val_loss / len(val_loader)
+            val_metric += metric.item()
+
+    return val_loss / len(val_loader), val_metric / len(val_loader)
 
 
 def train(
@@ -57,21 +77,27 @@ def train(
     train_loader: DataLoader,
     val_loader: DataLoader,
     loss_fn: nn.Module,
+    metric_fn: nn.Module,
     optimizer: optim.Optimizer,
     device: torch.device,
     epochs=10,
+    metric_name="MAE",
 ):
-    history = {"train_loss": [], "val_loss": []}
+    history = defaultdict(list)
 
     for epoch in range(epochs):
-        train_loss = train_epoch(model, train_loader, loss_fn, optimizer, device)
-        val_loss = val_epoch(model, val_loader, loss_fn, device)
+        train_loss, train_metric = train_epoch(
+            model, train_loader, loss_fn, metric_fn, optimizer, device
+        )
+        val_loss, val_metric = val_epoch(model, val_loader, loss_fn, metric_fn, device)
 
         history["train_loss"].append(train_loss)
+        history["train_metric"].append(train_metric)
         history["val_loss"].append(val_loss)
+        history["val_metric"].append(val_metric)
 
         print(
-            f"\nEpoch {epoch+1}/{epochs}, Train Loss: {train_loss:.4f}, Validation Loss: {val_loss:.4f}\n"
+            f"\nEpoch {epoch+1}/{epochs}, Train - Loss: {train_loss:.4f}, {metric_name}: {train_metric:.4f} | Validation - Loss: {val_loss:.4f}, {metric_name}: {val_metric:.4f}\n"
         )
         print("-" * 100)
 
@@ -79,17 +105,29 @@ def train(
 
 
 def test(
-    model: nn.Module, test_loader: DataLoader, loss_fn: nn.Module, device: torch.device
+    model: nn.Module,
+    test_loader: DataLoader,
+    loss_fn: nn.Module,
+    metric_fn: nn.Module,
+    device: torch.device,
 ):
     model.eval()
     test_loss = 0.0
+    test_metric = 0.0
+
     with torch.no_grad():
         for X, y in tqdm(test_loader, "test_batch"):
             X, y = X.to(device), y.to(device)
+
             y_pred = model(X)
+
             loss = loss_fn(y_pred, y)
+            metric = metric_fn(y_pred, y)
+
             test_loss += loss.item()
-    return test_loss / len(test_loader)
+            test_metric += metric.item()
+
+    return test_loss / len(test_loader), test_metric / len(test_loader)
 
 
 def get_data(generator: torch.Generator) -> Tuple[DataLoader, DataLoader]:
@@ -99,8 +137,8 @@ def get_data(generator: torch.Generator) -> Tuple[DataLoader, DataLoader]:
             transforms.RandomResizedCrop(224, scale=(0.9, 1.0)),
             transforms.ColorJitter(0.5, 0.5, 0.5, 0.5),
             transforms.GaussianBlur(5, sigma=(0.1, 2.0)),
-            #transforms.RandomHorizontalFlip(), #should not be here since the model would confuse left and right
-			transforms.Grayscale(),
+            # transforms.RandomHorizontalFlip(), #should not be here since the model would confuse left and right
+            transforms.Grayscale(),
             transforms.ToDtype(torch.float32, scale=True),
         ]
     )
@@ -108,11 +146,6 @@ def get_data(generator: torch.Generator) -> Tuple[DataLoader, DataLoader]:
     ds = JetbotDataset(
         DATA_PATH, transform, shift=5
     )  # predict the move 5 frames later and 10 framers later
-
-    # for i in [182,2137,4312]:
-    #     img,lab = ds[i]
-    #     cv2.imshow("in", cv2.cvtColor(np.transpose(img.numpy(),(1,2,0)),cv2.COLOR_BGR2RGB))
-    #     cv2.waitKey(0)
 
     train_set, test_set = random_split(ds, [0.8, 0.2], generator=generator)
 
@@ -122,40 +155,53 @@ def get_data(generator: torch.Generator) -> Tuple[DataLoader, DataLoader]:
 
 
 if __name__ == "__main__":
-	if torch.backends.mps.is_available():
-		device = torch.device("mps")
-	elif torch.cuda.is_available():
-		device = torch.device("cuda")
-	else:
-		device = torch.device("cpu")
+    if torch.backends.mps.is_available():
+        device = torch.device("mps")
+    elif torch.cuda.is_available():
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
 
-	generator = torch.Generator().manual_seed(42)
-	train_loader, test_loader = get_data(generator)
-	model = ConvNet([1, 16, 32, 48, 64], [64 * 10 * 10, 64], 6)
-	model.to(device)
-	loss_fn = WeightedMSELoss(weights=torch.tensor([2,10]*3,dtype=torch.float32).to(device))#nn.L1Loss() # output is between -1 and 1, so when the difference is smaller than 1 the MSE actually makes it smaller
-	optimizer = optim.SGD(model.parameters(), lr=0.001)
-	summary(model, (1, 224, 224))
+    generator = torch.Generator().manual_seed(42)
+    train_loader, test_loader = get_data(generator)
 
-	history = train(
-		model, train_loader, test_loader, loss_fn, optimizer, device, epochs=10
-	)
+    model = ConvNet([1, 16, 32, 48, 64], [64 * 10 * 10, 64], 6).to(device)
 
-	test_loss = test(model, test_loader, loss_fn, device)
+    metric_fn = MeanAbsoluteError().to(device)
+    loss_fn = WeightedMSELoss(
+        weights=torch.tensor([2, 10] * 3, dtype=torch.float32).to(device)
+    )  # nn.L1Loss() # output is between -1 and 1, so when the difference is smaller than 1 the MSE actually makes it smaller
 
+    optimizer = optim.SGD(model.parameters(), lr=0.001)
 
-	print(f"Test Loss: {test_loss:.4f}")
+    history = train(
+        model,
+        train_loader,
+        test_loader,
+        loss_fn,
+        metric_fn,
+        optimizer,
+        device,
+        epochs=15,
+    )
 
-	torch_input = torch.randn(*(1,1,224,224)).to(device)
+    test_loss, test_metric = test(model, test_loader, loss_fn, metric_fn, device)
 
-	torch.onnx.export(
-		model,                      # model to be exported
-		torch_input,                # sample input tensor
-		"./model.onnx",             # where to save the ONNX file
-		export_params=True,         # store the trained parameter weights inside the model file
-		opset_version=11,           # specify the ONNX opset version
-		do_constant_folding=True,   # perform constant folding for optimization
-		input_names=['input'],      # the model's input names
-		output_names=['output'],    # the model's output names
-		dynamic_axes={'input': {0: 'batch_size'}, 'output': {0: 'batch_size'}}  # dynamic axes for variable batch size
-	)
+    print(f"Test - Loss: {test_loss:.4f}, MAE: {test_metric:.4f}")
+
+    torch_input = torch.randn(*(1, 1, 224, 224)).to(device)
+
+    torch.onnx.export(
+        model,  # model to be exported
+        torch_input,  # sample input tensor
+        "./model.onnx",  # where to save the ONNX file
+        export_params=True,  # store the trained parameter weights inside the model file
+        opset_version=11,  # specify the ONNX opset version
+        do_constant_folding=True,  # perform constant folding for optimization
+        input_names=["input"],  # the model's input names
+        output_names=["output"],  # the model's output names
+        dynamic_axes={
+            "input": {0: "batch_size"},
+            "output": {0: "batch_size"},
+        },  # dynamic axes for variable batch size
+    )
